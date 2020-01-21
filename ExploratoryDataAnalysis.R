96847039e4d027d892387f822c2f6712fa3bd1e7
cocoscodes <solis_acevedo@hotmail.com>
2020-01-21 17:22:18 +0100

    latest 200121

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -10,4 +10,19 @@ getwd()
 # 5.- describe and document the evidence - the data graphic shoudl tell the complete story
 # 6.- content is king - quality, relevance, and integrity of the content makes a succesfull presentation
 
+# Exploratory graphs ----
+# exploratory grpahs are ro personally understand the data, not for presentation purposes
 
+pollution <- read.csv("avgpm25.csv", colClasses = c("numeric", "character", "factor", "numeric", "numeric"))
+head(pollution)
+summary(pollution$pm25)
+
+boxplot(pollution$pm25, col = "blue")
+abline(h=12) # shows the line where the upper limit lies for the EPA
+
+hist(pollution$pm25, col = "green", breaks = 100) # breaks adds the number per column - size
+rug(pollution$pm25) # adds exac location in the histogram undernead
+
+hist(pollution$pm25, col = "green")
+abline(v=12, lwd=2)
+abline(v=median(pollution$pm25), col= "magenta",lwd=4)
