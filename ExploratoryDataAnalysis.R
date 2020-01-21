0d2f2e721277b97bc01ffd64512a4b53a5a7a621
cocoscodes <solis_acevedo@hotmail.com>
2020-01-21 15:07:11 +0100

    latest 200121

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -1,1 +1,13 @@
 getwd()
+
+# Principles of Analytics Graphics ----
+
+# 1.- always ask "compared to what?" - the result you observe is in relation to what?
+# 2.- what is your causal framework for thinking about a question? - show causale mechanism of the observation
+# 3.- show multivariate data, the real world is multivariate, more than 2 variables - show are many variables as possible 
+# simpsons paradox -is a phenomenon in probability and statistics, in which a trend appears in several different groups of data but disappears or reverses when these groups are combined.
+# 4.- intergation of evidence - integrate words, numbers, images, diagrams, etc.
+# 5.- describe and document the evidence - the data graphic shoudl tell the complete story
+# 6.- content is king - quality, relevance, and integrity of the content makes a succesfull presentation
+
+
